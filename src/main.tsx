e8c72c15ff80e056f37c4869ece8d76d3f94c8e0
ifcalm <ifcalm.ok@gmail.com>
2026-06-23 20:35:29 +0800

    调整字体

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@fontsource-variable/inter/index.css'
 import '@fontsource-variable/manrope/index.css'
+import '@fontsource/libre-caslon-display/index.css'
 import './index.css'
 import App from './App'
 
